dd0cf4db590c36b8a3f71e0af8238d7c766da133
aka-nick <bechalrang@gmail.com>
2023-03-11 11:15:28 +0900

    섹션6 - 스프링 예외 추상화 적용 - 완료

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -7,6 +7,7 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
 import hello.jdbc.repository.MemberRepositoryV3;
 import hello.jdbc.repository.MemberRepositoryV4_1;
+import hello.jdbc.repository.MemberRepositoryV4_2;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
@@ -57,8 +58,8 @@ class MemberServiceV4Test {
         }
 
         @Bean
-        MemberRepositoryV4_1 memberRepository() {
-            return new MemberRepositoryV4_1(dataSource);
+        MemberRepositoryV4_2 memberRepository() {
+            return new MemberRepositoryV4_2(dataSource);
         }
 
         @Bean
