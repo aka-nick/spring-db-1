8e1b293a4190a6e13f67ab6060f368dc5939e8f2
aka-nick <bechalrang@gmail.com>
2023-03-11 11:09:23 +0900

    섹션6 - 스프링 예외 추상화 적용 - 스프링예외 테스트 코드 적용

--- src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -16,6 +16,7 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 
 @Slf4j
 public class SpringExceptionTranslatorTest {
@@ -53,8 +54,9 @@ public class SpringExceptionTranslatorTest {
             pstmt.executeQuery();
         }
         catch (SQLException e) {
+            assertThat(e.getErrorCode()).isEqualTo(42122);
             // 스프링이 제공하는 예외변환기
-            SQLErrorCodeSQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator();
+            SQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
 
             // 스프링이 제공하는 변환예외 인터페이스
             DataAccessException resultEx = translator.translate("select", sql, e);
@@ -63,8 +65,6 @@ public class SpringExceptionTranslatorTest {
             // 기대되는 스프링 예외인터페이스 구현체 : 잘못된 문법 예외(BadSqlGrammarException)
             assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
 
-            // 직접 래핑하는 대신, 스프링이 대신 변환한 예`외를 던진다 - 직접 예외 변환할 필요 사라짐!
-            throw resultEx;
             /*
             이런 기술을 사용하게 되면 스프링에 대한 종속성이 생기게 된다.
             하지만 예외추상화까지 상세히 다뤄주는 다른 기술이 쓸만한 수준에서는 없다고 보면 되기 때문에
